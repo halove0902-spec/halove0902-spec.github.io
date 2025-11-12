989261125f7cad0011e2b41f6d124129ae9c2d7a
Mu-jun <suo24691@gmail.com>
2025-11-13 01:13:55 +0900

    '/' 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Layout from './routes/Layout';
 function App() {
   return (
     <>
-      <BrowserRouter basename="tyca">
+      <BrowserRouter basename="/tyca">
         <Routes>
           <Route path="/" element={<Layout />} />
         </Routes>
