889dc902ca82a616da6b8a6c734d46239b7cecf3
Mu-jun <suo24691@gmail.com>
2026-03-20 00:28:47 +0900

    희망하는 상담사 추가

--- src/routes/Contact/index.tsx
+++ src/routes/Contact/index.tsx
@@ -9,6 +9,7 @@ function Contact() {
   const [name, setName] = useState('');
   const [contact, setContact] = useState('');
   const [message, setMessage] = useState('');
+  const [counselor, setCounselor] = useState('');
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,14 +18,17 @@ function Contact() {
     setSubmit(true);
 
     try {
-      const response = await contactSubmit(name, contact, message);
+      const response = await contactSubmit(name, contact, message, counselor);
 
       const res = await response.json();
       console.log(response);
       console.log(res);
       if (response.ok) {
         alert('전송되었습니다');
-        window.location.reload();
+        setName('');
+        setContact('');
+        setMessage('');
+        setCounselor('');
       } else {
         alert('Error: ' + res.error);
       }
@@ -37,39 +41,58 @@ function Contact() {
   };
   return (
     <div className="main-container">
-      <div className="max-w-2xl">
+      <div className="max-w-2xl w-full">
         <h4>상담 신청하기</h4>
         <p>상담을 신청하시면 확인 후 연락드리겠습니다.</p>
 
         <hr className="my-8" />
 
         <form className="text-black" onSubmit={onSubmit}>
-          <label htmlFor="name">이름</label>
-          <input
-            type="text"
-            className="w-8"
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label htmlFor="contact">연락처(핸드폰 or 이메일)</label>
-          <input
-            type="text"
-            required
-            value={contact}
-            onChange={(e) => setContact(e.target.value)}
-          />
-          <label htmlFor="message">내용</label>
-          <textarea
-            rows={4}
-            cols={50}
-            required
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-          <button className="btn primary" type="submit" disabled={isSubmit}>
-            {isSubmit ? '전송중...' : '제출'}
-          </button>
+          <div className="form-area">
+            <label htmlFor="name">신청자 이름</label>
+            <input
+              type="text"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="form-area">
+            <label htmlFor="contact">신청자 연락처(핸드폰 or 이메일)</label>
+            <input
+              type="text"
+              required
+              value={contact}
+              onChange={(e) => setContact(e.target.value)}
+            />
+          </div>
+
+          <div className="form-area">
+            <label htmlFor="message">신청 내용</label>
+            <textarea
+              rows={4}
+              cols={50}
+              required
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </div>
+
+          <div className="form-area">
+            <label htmlFor="counselor">희망하는 상담사 (선택사항)</label>
+            <input
+              type="text"
+              value={counselor}
+              onChange={(e) => setCounselor(e.target.value)}
+            />
+          </div>
+
+          <div className="mt-5">
+            <button className="btn primary" type="submit" disabled={isSubmit}>
+              {isSubmit ? '전송중...' : '제출'}
+            </button>
+          </div>
         </form>
       </div>
     </div>
--- src/util/contact.ts
+++ src/util/contact.ts
@@ -1,14 +1,16 @@
 const contactUrl: string =
-  'https://script.google.com/macros/s/AKfycbyBZILoSEIB5LEIdMmB2p_Zdt_ruc8UjZTU88zchqV6zg66-FqVPU3gMmXV1mYb-yN4/exec';
+  'https://script.google.com/macros/s/AKfycbwixFQo2kekMPaPsbq9G8G5UlHi0L0SMKbJ1l7GJB3TQxIIluvjffOTzmzt8ZUqHkNs/exec';
 
 export function contactSubmit(
   name: string,
   contact: string,
-  message: string
+  message: string,
+  counselor: string,
 ): Promise<Response> {
   const formData = new FormData();
   formData.append('name', name);
   formData.append('contact', contact);
+  formData.append('counselor', counselor);
   formData.append('message', message);
 
   return fetch(contactUrl, {
